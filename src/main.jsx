84e616690382ba7dcac4ce5266b3122e4fc5550a
richard amofa <amofarichard101@gmail.com>
2026-09-23 15:58:27 +0000

    rm analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/next"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -7,6 +6,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 )
